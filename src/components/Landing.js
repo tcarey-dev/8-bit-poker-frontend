4800ae1374593d564a1ea7fcd315b1708bacdf08
aaronsocks <aasaccurato@gmail.com>
2023-06-29 16:24:42 -0700

    landing page css style finish

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import { useNavigate } from 'react-router-dom';
 import './Landing.css';
+import "nes.css/css/nes.min.css";
 
 function Landing(){
 
@@ -9,18 +10,20 @@ function Landing(){
 return(<>
     <section id="imageContainer">
         <div>
-            <h1>Welcome to<br/>8-Bit Poker Showdown!</h1>
+            <header>
+                <h1>Welcome to<br/>8-Bit Poker Showdown!</h1>
+            </header>
         </div>
     </section>
-    <section>
+    <section id="buttonContainer">
         <h2>Select Option Below</h2>
-        <button className="btn btn-primary btn-lg" >
-            <i className="bi btn-primary btn-lg" 
+        <button className="nes-btn is-primary" >
+            <i 
             // onClick={() => navigate()}
             ></i>Login
         </button>
-        <button className="btn btn-primary btn-lg" >
-            <i className="bi btn-primary btn-lg"></i>Sign-Up
+        <button className="nes-btn is-primary" >
+            <i></i>Sign-Up
         </button>
     </section>
 </>)
