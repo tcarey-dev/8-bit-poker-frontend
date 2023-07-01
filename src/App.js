792784eb262a51baadea92613d9427a54fa47888
aaronsocks <aasaccurato@gmail.com>
2023-07-01 10:49:21 -0700

    updated app and lobby

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Landing from "./components/Landing";
 import NotFound from "./components/NotFound";
 import Lobby from "./components/Lobby";
-import Navbar from "./components/NavBar";
+import LoginForm from "./components/Login";
+import RoomForm from "./components/RoomForm";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Landing/>}/>
+        <Route path='/room/create' element={<RoomForm/>}/>
+        <Route path='/room/update/:id' element={<RoomForm/>}/>
 	      <Route path='/lobby' element={<Lobby/>}/>
 	      <Route path='*' element={<NotFound/>}/>
       </Routes>
--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import "./lobby.css";
 import "nes.css/css/nes.min.css";
-import Navbar from './NavBar';
-
+import Errors from "../Errors";
 
 function Lobby(){
     const [rooms, setRooms] = useState([]);
+    const [errors, setErrors] = useState([]);
     const navigate = useNavigate();
     const url = "http://localhost:8080/api/room";
 
@@ -23,11 +23,30 @@ function Lobby(){
         .catch(console.log);
     }, []);
 
+    function handleDeleteRoom(roomId){
+        const room = rooms.find(room => room.roomId === roomId);
+        if(window.confirm(`Delete room: ${room.roomId}, with ${room.seats} seats and $${room.stake} stakes?`)){
+            const init = {
+                method: 'DELETE'
+            };
+    
+        fetch(`${url}/${roomId}`, init)
+        .then(response => {
+            if(response.status === 204){
+                const newRooms = rooms.filter(room => room.roomId !== roomId);
+                setRooms(newRooms);
+            }else{
+                return Promise.reject(`Unexpected Status Code: ${response.status}`);
+            }
+        })
+        .catch(err => setErrors(err));
+    }
+}
+
     return(
         <>
-        <Navbar/>
         <div id="listBorder" className="nes-container is-centered is-rounded">
-            <h2 class="title">Rooms</h2>
+            <h2 className="title">Rooms</h2>
                 <div id="listContainer" className="nes-table-responsive">
                     <table className="nes-table is-bordered is-centered is-dark">
                         <thead>
@@ -48,6 +67,12 @@ function Lobby(){
                                         <Link className="nes-btn is-success" type="button" to={"/room"}>
                                             JOIN <i className="nes-icon coin is-small"/>
                                         </Link>
+                                        <Link className="nes-btn is-warning" to={`/room/update/${room.roomId}`}>
+                                            UPDATE
+                                        </Link>
+                                        <button className="nes-btn is-error" onClick={() => handleDeleteRoom(room.roomId)}>
+                                            DELETE
+                                        </button>
                                     </td>
                                 </tr>
                             ))}
