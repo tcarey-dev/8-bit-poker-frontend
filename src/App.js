db00586440978fc59dd303c772a9779b14b9b3ac
xiaofne <92196198+xiaofne@users.noreply.github.com>
2023-07-01 00:03:53 -0400

    roomForm mising update,just have to update paths

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
+// import { Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import Landing from "./components/Landing";
+// import NotFound from "./components/NotFound";
+// import Lobby from "./components/Lobby";
+
+// function App() {
+//   return (
+//   <>
+//     <Router>
+//       <Routes>
+//         <Route path='/' element={<Landing/>}/>
+// 	      <Route path='/lobby' element={<Lobby/>}/>
+// 	      <Route path='*' element={<NotFound/>}/>
+//       </Routes>
+//     </Router>
+//   </>
+//   );
+// }
+
+// export default App;
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Landing from "./components/Landing";
 import NotFound from "./components/NotFound";
 import Lobby from "./components/Lobby";
+import RoomForm from "./components/RoomForm";
 
 function App() {
   return (
   <>
     <Router>
       <Routes>
-        <Route path='/' element={<Landing/>}/>
+        <Route path='/' element={<RoomForm/>}/>
+        <Route path='/rooms/edit/:roomId' element={<RoomForm/>}/>
 	      <Route path='/lobby' element={<Lobby/>}/>
 	      <Route path='*' element={<NotFound/>}/>
       </Routes>
